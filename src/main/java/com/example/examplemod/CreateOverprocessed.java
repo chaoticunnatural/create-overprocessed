69498b7e4cf3280361f6ef7ffdab1058ec67883c
yahaha5788 <j.thomps1310@gmail.com>
2026-01-19 21:06:31 -0500

    here we go again

---
+++ src/main/java/com/example/examplemod/CreateOverprocessed.java
@@ -0,0 +1,32 @@
+package com.example.examplemod;
+
+import com.mojang.logging.LogUtils;
+import com.simibubi.create.foundation.data.CreateRegistrate;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(CreateOverprocessed.MODID)
+public class CreateOverprocessed
+{
+    public static final String MODID = "create_overprocessed";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    private static final CreateRegistrate REGISTRATE = CreateRegistrate.create(MODID);
+
+
+    public CreateOverprocessed()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        REGISTRATE.registerEventListeners(modEventBus);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    public static ResourceLocation asResource(String path) {
+        return new ResourceLocation(MODID, path);
+    }
+}
